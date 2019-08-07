c35a9a080e0f87cc1874caf6b45fb15d56c25373
Tim Neumann <neumantm@fius.informatik.uni-stuttgart.de>
2019-08-07 20:22:02 +0200

    Add empty simulation module.

---
+++ ICGE-Simulation/src/main/java/de/unistuttgart/informatik/fius/icge/simulation/Simulation.java
@@ -0,0 +1,19 @@
+/*
+ * This source file is part of the FIUS ICGE project.
+ * For more information see github.com/FIUS/ICGE2
+ *
+ * Copyright (c) 2019 the ICGE project authors.
+ * 
+ * This software is available under the MIT license.
+ * SPDX-License-Identifier:    MIT
+ */
+package de.unistuttgart.informatik.fius.icge.simulation;
+
+/**
+ * The interface for the main simulation of the ICGE.
+ * 
+ * @author Tim Neumann
+ */
+public interface Simulation {
+    
+}
---
+++ ICGE-Simulation/src/main/java/module-info.java
@@ -0,0 +1,1 @@
+module de.unistuttgart.informatik.fius.icge.simulation {}
