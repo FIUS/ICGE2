b15c077c120449ec5f06f6319acc58eaac119ae0
Tim Neumann <neumantm@fius.informatik.uni-stuttgart.de>
2019-08-30 14:15:21 +0200

    Fix type in example package.

--- examples/mario/src/main/java/de/unistauttgart/informatik/fius/icge/example/mario/entity/Wall.java
+++ examples/mario/src/main/java/de/unistauttgart/informatik/fius/icge/example/mario/entity/Wall.java
@@ -7,9 +7,9 @@
  * @version 1.0.0
  *
  */
-package de.unistauttgart.informatil.fius.icge.example.mario.entity;
+package de.unistauttgart.informatik.fius.icge.example.mario.entity;
 
-import de.unistauttgart.informatil.fius.icge.example.mario.Texture;
+import de.unistauttgart.informatik.fius.icge.example.mario.Texture;
 import de.unistuttgart.informatik.fius.icge.simulation.entity.BasicEntity;
 
 /**
