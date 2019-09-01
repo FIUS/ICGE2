160599d48f8de210d4f91d3892f6ddd4a67d7fa2
Tim Neumann <neumantm@fius.informatik.uni-stuttgart.de>
2019-09-01 17:04:58 +0200

    Add ICGE Log module.

---
+++ ICGE-Log/src/main/java/de/unistuttgart/informatik/fius/icge/log/Logger.java
@@ -0,0 +1,18 @@
+/*
+ * ICGE-Log
+ *
+ * TODO: Project Beschreibung
+ *
+ * @author Tim Neumann
+ * @version 1.0.0
+ *
+ */
+package de.unistuttgart.informatik.fius.icge.log;
+
+/**
+ * TODO: Description
+ * @author Tim Neumann
+ */
+public class Logger {
+    
+}
