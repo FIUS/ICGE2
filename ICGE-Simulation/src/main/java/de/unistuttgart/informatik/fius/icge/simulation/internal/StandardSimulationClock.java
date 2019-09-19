a9f3193b0f022c121916b31e4cf9e3ff6dd89714
Tobias Wältken <tobias.w@ltken.de>
2019-09-19 08:27:08 +0200

    Add coment which was removed by the formatter

--- ICGE-Simulation/src/main/java/de/unistuttgart/informatik/fius/icge/simulation/internal/StandardSimulationClock.java
+++ ICGE-Simulation/src/main/java/de/unistuttgart/informatik/fius/icge/simulation/internal/StandardSimulationClock.java
@@ -156,7 +156,13 @@ public class StandardSimulationClock implements SimulationClock {
     public synchronized void step() {
         if (this.isRunning()) throw new TimerAlreadyRunning();
         
-        new Thread((Runnable) () -> StandardSimulationClock.this.tick(), "single-step").start();
+        new Thread(
+                () -> {
+                    //FIXME this only executes a graphics tick an not neccessariely
+                    // a simulation tick.
+                    StandardSimulationClock.this.tick();
+                }, "single-step"
+        ).start();
     }
     
     /**
