2051ea12e23fae07198631d6af1e741d4b35cb41
Tim Neumann <neumantm@fius.informatik.uni-stuttgart.de>
2019-09-28 15:10:03 +0200

    Apply defined header to all java files.

--- ICGE-ManualStart/src/main/java/module-info.java
+++ ICGE-ManualStart/src/main/java/module-info.java
@@ -1,3 +1,12 @@
+/*
+ * This source file is part of the FIUS ICGE project.
+ * For more information see github.com/FIUS/ICGE2
+ * 
+ * Copyright (c) 2019 the ICGE project authors.
+ * 
+ * This software is available under the MIT license.
+ * SPDX-License-Identifier:    MIT
+ */
 module de.unistuttgart.informatik.fius.icge.manualstart {
     requires de.unistuttgart.informatik.fius.icge.simulation;
 }
--- examples/mario/src/main/java/de/unistuttgart/informatik/fius/icge/example/mario/entity/Wall.java
+++ examples/mario/src/main/java/de/unistuttgart/informatik/fius/icge/example/mario/entity/Wall.java
@@ -1,11 +1,11 @@
 /*
- * ICGE-Example-Mario
- *
- * TODO: Project Beschreibung
- *
- * @author Tim Neumann
- * @version 1.0.0
- *
+ * This source file is part of the FIUS ICGE project.
+ * For more information see github.com/FIUS/ICGE2
+ * 
+ * Copyright (c) 2019 the ICGE project authors.
+ * 
+ * This software is available under the MIT license.
+ * SPDX-License-Identifier:    MIT
  */
 package de.unistuttgart.informatik.fius.icge.example.mario.entity;
 
