27245659019f8ab86b1e02d6fb9e9a3d10ed15f8
Tim Neumann <neumantm@fius.informatik.uni-stuttgart.de>
2019-09-30 16:00:31 +0200

    Add version bump capabilities.

--- scriptsAndTools/installCiRequiredTools.sh
+++ scriptsAndTools/installCiRequiredTools.sh
@@ -10,4 +10,9 @@ function fail {
 sudo apt-get update
 sudo apt-get install -y xmlstarlet
 
+eval "$(ssh-agent -s)"
 openssl aes-256-cbc -K $encrypted_6c62aa4934cd_key -iv $encrypted_6c62aa4934cd_iv -in "$dir/deployKey.enc" -out "$dir/deployKey" -d
+chmod 600 "$dir/deployKey"
+ssh-add "$dir/deployKey"
+
+cp "$dir/m2settings.xml" "~/.m2/settings.xml"
