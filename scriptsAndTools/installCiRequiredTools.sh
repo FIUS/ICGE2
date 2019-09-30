ca0dd75b0223f77981369b23b0bdbcdada8c8445
Tim Neumann <neumantm@fius.informatik.uni-stuttgart.de>
2019-09-30 16:16:57 +0200

    Make sure m2 directory is there.

--- scriptsAndTools/installCiRequiredTools.sh
+++ scriptsAndTools/installCiRequiredTools.sh
@@ -15,4 +15,5 @@ openssl aes-256-cbc -K $encrypted_6c62aa4934cd_key -iv $encrypted_6c62aa4934cd_i
 chmod 600 "$dir/deployKey"
 ssh-add "$dir/deployKey"
 
+mkdir "~/.m2"
 cp "$dir/m2settings.xml" "~/.m2/settings.xml"
