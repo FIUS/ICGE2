6623484ccaeaf6468031359c6c24356da42bdb8c
Tim Neumann <neumantm@fius.informatik.uni-stuttgart.de>
2019-09-30 15:49:58 +0200

    Add deployKey.

--- scriptsAndTools/installCiRequiredTools.sh
+++ scriptsAndTools/installCiRequiredTools.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
 
+dir="$(dirname "$(realpath "$0")")"
+
+function fail {
+  echo $1
+  exit $2
+}
+
 sudo apt-get update
 sudo apt-get install -y xmlstarlet
+
+openssl aes-256-cbc -K $encrypted_6c62aa4934cd_key -iv $encrypted_6c62aa4934cd_iv -in "$dir/deployKey.enc" -out "$dir/deployKey" -d
