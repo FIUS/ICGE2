b4adb7087e14e135af8b6954c1ff1d886814c76a
Tim Neumann <neumantm@fius.informatik.uni-stuttgart.de>
2019-10-01 20:01:49 +0200

    Fix ci to use git over ssh.

--- scriptsAndTools/installCiRequiredTools.sh
+++ scriptsAndTools/installCiRequiredTools.sh
@@ -14,3 +14,5 @@ eval "$(ssh-agent -s)"
 openssl aes-256-cbc -K $encrypted_6c62aa4934cd_key -iv $encrypted_6c62aa4934cd_iv -in "$dir/deployKey.enc" -out "$dir/deployKey" -d
 chmod 600 "$dir/deployKey"
 ssh-add "$dir/deployKey"
+
+git remote set-url origin git@github.com:FIUS/ICGE2.git
