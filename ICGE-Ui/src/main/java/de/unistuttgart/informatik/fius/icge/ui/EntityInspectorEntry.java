a72c9f12740911a5bc8b8042aa206b29362ed854
Tobias Wältken <tobias.w@ltken.de>
2019-10-07 00:35:41 +0200

    Add javadoc

--- ICGE-Ui/src/main/java/de/unistuttgart/informatik/fius/icge/ui/EntityInspectorEntry.java
+++ ICGE-Ui/src/main/java/de/unistuttgart/informatik/fius/icge/ui/EntityInspectorEntry.java
@@ -25,6 +25,18 @@ public class EntityInspectorEntry {
     
     private final Consumer<String> callback;
     
+    /**
+     * Default constructor
+     * 
+     * @param name
+     *     The name of the setting
+     * @param type
+     *     The type of the setting
+     * @param value
+     *     The start value of the setting
+     * @param callback
+     *     The labda wich gets called when the value changes
+     */
     public EntityInspectorEntry(final String name, final String type, final String value, final Consumer<String> callback) {
         this.name = name;
         this.type = type;
@@ -32,18 +44,39 @@ public class EntityInspectorEntry {
         this.callback = callback;
     }
     
+    /**
+     * Getter for the name
+     * 
+     * @return returns the name
+     */
     public String getName() {
         return this.name;
     }
     
+    /**
+     * Getter for the type
+     * 
+     * @return returns the type
+     */
     public String getType() {
         return this.type;
     }
     
+    /**
+     * Getter for the value
+     * 
+     * @return returns the value
+     */
     public String getValue() {
         return this.value;
     }
     
+    /**
+     * This function runs the callback of the item
+     * 
+     * @param value
+     *     The argument of the callback
+     */
     public void runCallback(String value) {
         this.callback.accept(value);
     }
