4389bb0b1bc7c94dd3d2df5338663bb416d498ac
Tim Neumann <neumantm@fius.informatik.uni-stuttgart.de>
2019-10-07 17:25:38 +0200

    Fix bug in ci

--- scriptsAndTools/deployGhPages.sh
+++ scriptsAndTools/deployGhPages.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
 dir="$(dirname "$(realpath "$0")")"
 
+function fail {
+  echo $1
+  exit $2
+}
+
+if [[ "$TRAVIS_BRANCH" != "master" ]] ;then
+  fail "Not on master" 1
+fi
+
 source "$dir/prepareGitForPush.sh"
 
-git clone --depth 1 -b gh-pages git@github.com:FIUS/JVK-2019.git gh-pages
+git clone --depth 1 -b gh-pages git@github.com:FIUS/ICGE2.git gh-pages
 
 mvn javadoc:aggregate -pl '!ICGE-build-tools,!ICGE-ManualStart'
 cp -r target/site/apidocs/ gh-pages
