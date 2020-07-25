98abf1cfb8f0ded6a58917af6412df9928f54614
Tim Neumann <neumantm@fius.informatik.uni-stuttgart.de>
2020-07-25 21:17:15 +0200

    Add verbose check

--- scriptsAndTools/checkFormat.sh
+++ scriptsAndTools/checkFormat.sh
@@ -7,8 +7,8 @@ function fail {
   exit $2
 }
 
-if [ $# -ne 0 ] ; then
-  fail "No argument expected" 2
+if [ ! "$1" == "-v" ] && [ $# -ne 0 ] ; then
+  fail "No argument expected (or single -v)" 2
 fi
 
 if ! which git >/dev/null 2>&1 ;then
@@ -31,10 +31,18 @@ if ! "$dir/checkPomFormat.sh" ;then
   fail "The format of a pom is wrong." 21
 fi
 
-mvn -B -f ICGE-build-tools install > /dev/null 2>&1
+if [ "$1" == "-v" ] ;then
+  mvn -B -f ICGE-build-tools install
 
-if ! mvn -B formatter:validate >/dev/null 2>&1 ;then
-  fail "The format of a java file is wrong. See mvn formatter:validate" 22
+  if ! mvn -B formatter:validate ;then
+    fail "The format of a java file is wrong. See mvn formatter:validate" 22
+  fi
+else
+  mvn -B -f ICGE-build-tools install > /dev/null
+
+  if ! mvn -B formatter:validate >/dev/null ;then
+    fail "The format of a java file is wrong. See mvn formatter:validate" 22
+  fi
 fi
 
 if ! "$dir/checkJavaFileComments.sh" ;then
