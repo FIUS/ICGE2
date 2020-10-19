2ed9aa91ba823c3d59f133eebed22c5aaa221803
Fabian Bühler <17296905+buehlefs@users.noreply.github.com>
2020-10-19 14:56:03 +0200

    Fix missing licence header

--- ICGE-ManualStart/src/main/java/de/unistuttgart/informatik/fius/icge/manualstart/Coin.java
+++ ICGE-ManualStart/src/main/java/de/unistuttgart/informatik/fius/icge/manualstart/Coin.java
@@ -1,3 +1,12 @@
+/*
+ * This source file is part of the FIUS ICGE project.
+ * For more information see github.com/FIUS/ICGE2
+ * 
+ * Copyright (c) 2019 the ICGE project authors.
+ * 
+ * This software is available under the MIT license.
+ * SPDX-License-Identifier:    MIT
+ */
 package de.unistuttgart.informatik.fius.icge.manualstart;
 
 import de.unistuttgart.informatik.fius.icge.simulation.entity.BasicEntity;
